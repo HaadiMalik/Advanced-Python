ac7f0ca243e18ac194c0e5a63166c83f95dead91
HaadiMalik <frantic438@gmail.com>
2024-09-03 22:01:09 -0400

    Finish TODOs

--- Start/Ch1/sortingdata.py
+++ Start/Ch1/sortingdata.py
@@ -13,9 +13,16 @@ dataset = list(filter(lambda d: d['snow'] > 0.0, weatherdata))
 
 # sort the entire data set by how much snowfall there was
 # TODO: method 1: use the sorted() function to create a new list
-
+# sorted_dataset = sorted(dataset, key=lambda d: d['snow'], reverse=True)
+# print(sorted_dataset)
 
 # TODO: method 2: use the sort() function that every list has to sort in-place
-
+dataset.sort(key=lambda d: d['snow'], reverse=False)
+# print(dataset)
+# for day in dataset:
+#     print(f"{day['date']} had {day['snow']} inches of snow")
 
 # TODO: Sort on multiple fields: first by snowfall, then by average wind speed
+sorted_dataset = sorted(dataset, key=lambda d: (d['snow'], d['awnd']))
+for day in sorted_dataset:
+    print(f"{day['date']} had {day['snow']} inches of snow and {day['awnd']} mph wind")
